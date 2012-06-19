d7efc283ab78e5cd546bd55a2e6849e17a69f97f
Reese Morgan <rmorgan@spyderbytedesign.com>
2012-06-19 14:22:33 -0400

    changed function names to match theme name

--- theme-settings.php
+++ theme-settings.php
@@ -2,16 +2,16 @@
 
 /**
  * USAGE:
- * 1 - To use this file replace "adaptivetheme_subtheme" with the name of
+ * 1 - To use this file replace "uclick_theme" with the name of
  *     your theme in the function below.
  * 2 - Set 'style_enable_schemes' to 'on' in your themes info file (its near the bottom of that file).
  * 2 - Build or un-comment the forms. The Style Schemes form is ready to use,
  *     just un-comment it.
  */
 
-// Replace 'adaptivetheme_subtheme' with your themes name, eg:
+// Replace 'uclick_theme' with your themes name, eg:
 // function your_themes_name_form_system_theme_settings_alter(&$form, &$form_state)
-function adaptivetheme_subtheme_form_system_theme_settings_alter(&$form, &$form_state)  {
+function uclick_theme_form_system_theme_settings_alter(&$form, &$form_state)  {
 
   // Style Schemes
   /* -- Delete this line if you want to use Style Schemes
